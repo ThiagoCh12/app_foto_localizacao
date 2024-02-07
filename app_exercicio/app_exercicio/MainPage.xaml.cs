aacb0abd99921941ce00468c86efba5d76361f9c
Aluno <Aluno@SNCTM202LABF09>
2024-02-06 22:08:09 -0300

    Adicionar arquivos de projeto.

---
+++ app_exercicio/app_exercicio/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace app_exercicio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnFoto_Clicked(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        public async Task MostrarMapa()
+        {
+            var location = await Geolocation.GetLocationAsync(new GeolocationRequest()
+            { DesiredAccuracy = GeolocationAccuracy.Best });
+            var locationinfo = new Location(location.Latitude, location.Longitude);
+            var options = new MapLaunchOptions { Name = "Meu local" };
+            await Map.OpenAsync(locationinfo, options);
+
+        }
+
+        private async void btnMapa_Clicked(object sender, EventArgs e)
+        {
+            await MostrarMapa();
+        }
+    }
+}
